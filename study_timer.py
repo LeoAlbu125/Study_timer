9df0f607f1ec208327631de46fc2b662e4f3e33b
LeoAlbu125 <leonardoaall125@gmail.com>
2021-03-31 21:51:56 -0300

    adcionei um relogio fixo no cmd

--- study_timer.py
+++ study_timer.py
@@ -5,6 +5,7 @@ from playsound import playsound
 import pathlib
 import keyboard as kb
 import time
+import sys
 
 sq = 0       #Conta as repeticoes do ciclo estudo & descanco
 
@@ -16,19 +17,39 @@ print(f"To cancel the clock press '{cancel_timer}' key")
 
 while not kb.is_pressed(cancel_timer):
 
-    t30 = 10
-    t5 = 10
+    t30 = 1800
+    t5 = 300
 
     while (t30 > 0 and not kb.is_pressed(cancel_timer)):
         t30 -= 1
+
+        t2 = time.time()
+
+        tf = int(t2-t0)
+
+        thr, tmin, tsec = tf//3600 , (tf%3600)//60, (tf%3600)%60
+
+        print("timer: {:02d}:{:02d}:{:02d}".format(thr,tmin,tsec),end="",flush=True)
+        sys.stdout.flush()
+        print("\r",end="",flush=True)
         time.sleep(1)
-    
+
     if t30 == 0:
         playsound(str(os.path.dirname(__file__))+r"\Beep\violin_jingle.wav")
-        print("teste")
-
+        
+        
     while (t5 > 0 and not kb.is_pressed(cancel_timer)):
         t5 -= 1
+        
+        t2 = time.time()
+
+        tf = int(t2-t0)
+
+        thr, tmin, tsec = tf//3600 , (tf%3600)//60, (tf%3600)%60
+
+        print("timer: {:02d}:{:02d}:{:02d}".format(thr,tmin,tsec),end="",flush=True)
+        sys.stdout.flush()
+        print("\r",end="",flush=True)
         time.sleep(1)
 
     if t30 == 0 and t5 == 0:    
