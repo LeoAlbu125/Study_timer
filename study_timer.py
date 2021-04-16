1c3999582cae98d9f392fd8a34c9d5734e43c3dc
LeoAlbu125 <leonardoaall125@gmail.com>
2021-04-16 20:14:20 -0300

    "agora o programa cria um arquivo especifico (de tempo de estudo) para a atividade escolhida"

--- study_timer.py
+++ study_timer.py
@@ -6,6 +6,15 @@ import pathlib
 import keyboard as kb
 import time
 import sys
+from datetime import date as dt
+
+type_study = input("options: reading, programing: ").lower().strip(" ")
+
+if type_study != ("reading" or "programing"):
+    type_study = "programing"
+
+
+print(dt.today())
 
 sq = 0       #Conta as repeticoes do ciclo estudo & descanco
 
@@ -17,7 +26,7 @@ print(f"To cancel the clock press '{cancel_timer}' key")
 
 while not kb.is_pressed(cancel_timer):
 
-    t30 = 1800
+    t30 = 1500
     t5 = 300
 
     while (t30 > 0 and not kb.is_pressed(cancel_timer)):
@@ -35,7 +44,7 @@ while not kb.is_pressed(cancel_timer):
         time.sleep(1)
 
     if t30 == 0:
-        playsound(str(os.path.dirname(__file__))+r"\Beep\violin_jingle.wav")
+        playsound(str(os.path.dirname(__file__))+r"\Beep\short.wav")
         
         
     while (t5 > 0 and not kb.is_pressed(cancel_timer)):
@@ -62,5 +71,14 @@ t2 = time.time()
 tf = int(t2-t0)
 
 thr, tmin, tsec = tf//3600 , (tf%3600)//60, (tf%3600)%60
+if type_study == "programing":
+
+    with open("Tempo_de_estudo_programacao.txt","a") as time_archive:
+        time_archive.write("{} : {:02d}:{:02d}:{:02d}\n".format(dt.today(),thr,tmin,tsec))
+
+elif type_study == "reading":
+
+    with open("Tempo_de_estudo_reading.txt","a") as time_archive1:
+        time_archive1.write("{} : {:02d}:{:02d}:{:02d}\n".format(dt.today(),thr,tmin,tsec))
 
 print("programa desativado. \n             Hr Mi Sec\ntempo total: {:02d}:{:02d}:{:02d}".format(thr,tmin,tsec))
